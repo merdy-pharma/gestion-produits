3bf2e99add3f5704e9a6c94e70bbf687182929a2
merdy-pharma <fifinalandu@gmail.com>
2026-01-28 13:54:53 +0100

    Updated Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -139,7 +139,7 @@ const Products: React.FC = () => {
             const movement = {
               product_id: editingProduct.id,
               type: diff > 0 ? "IN" : "OUT",
-              reason: "AJUSTEMENT",
+              reason: "REINITIALISATION",
               quantity: Math.abs(diff),
               comment: "Modification manuelle depuis fiche produit",
              // created_at: new Date().toISOString(),
