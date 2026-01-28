6d617f6b96d406480e44b22bc8f714375604b32e
merdy-pharma <fifinalandu@gmail.com>
2026-01-28 12:47:26 +0100

    Updated Sidebar.tsx

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -69,7 +69,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
           {/* Header */}
           <div className="flex items-center justify-between h-16 px-4 border-b">
             <div className="flex items-center">
-              <ShoppingBag className="h-8 w-8 text-primary-500" />
+              <Cross className="h-8 w-8 text-primary-500" />
               <span className="ml-2 text-xl font-semibold text-gray-800">MERDY PHARMA</span>
             </div>
             <button 
