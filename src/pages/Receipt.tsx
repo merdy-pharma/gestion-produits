09fc5f7e7cba141f0616008b4472bdcbb35a2adb
merdy-pharma <fifinalandu@gmail.com>
2026-01-29 12:24:29 +0100

    Updated Receipt.tsx

--- src/pages/Receipt.tsx
+++ src/pages/Receipt.tsx
@@ -47,7 +47,7 @@ const Receipt = forwardRef<HTMLDivElement, ReceiptProps>(
 
             .pos-receipt {
               width: 220px !important;
-              max-width: 80mm !important;
+              max-width: none !important;
               margin: 0 !important;
               padding: 4px !important;
               font-size: 10px !important;
