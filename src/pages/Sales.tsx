534c795903e1024e201cdefa7d63320d194adff3
merdy-pharma <fifinalandu@gmail.com>
2026-01-29 14:46:11 +0100

    Updated Sales.tsx

--- src/pages/Sales.tsx
+++ src/pages/Sales.tsx
@@ -642,7 +642,7 @@ const Sales: React.FC = () => {
     data-tip={
       isSubmitting
         ? "Validation en cours..."
-        : cart.length === 0 || !selectedPayment
+        : cart.length === 0 || !selectedPayment || !customerName.trim()
         ? "Veuillez ajouter un article, saisir le nom du client et/ou sélectionner un mode de paiement"
         : ""
     }
