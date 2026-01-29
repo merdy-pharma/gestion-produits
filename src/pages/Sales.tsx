d6816b0eab7645104fab8a40b9dfae14b5f366a7
merdy-pharma <fifinalandu@gmail.com>
2026-01-29 10:41:19 +0100

    Updated Sales.tsx

--- src/pages/Sales.tsx
+++ src/pages/Sales.tsx
@@ -332,7 +332,7 @@ const Sales: React.FC = () => {
 
     // 🔐 Appel RPC transactionnelle
     const { data: saleId, error } = await supabase.rpc(
-      "create_sale_transaction",
+      "create_sale_transaction_ok",
       {
         p_customer_id: selectedCustomerId,
         p_payment_method: selectedPayment,
