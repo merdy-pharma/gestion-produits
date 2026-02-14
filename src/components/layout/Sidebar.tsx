c0e57cbd90bc9df58a0df966489bb4daf0d9fc64
merdy-pharma <fifinalandu@gmail.com>
2026-02-14 19:02:46 +0100

    Updated Sidebar.tsx

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -61,7 +61,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
 
       {/* Sidebar */}
       <aside 
-        className={`fixed inset-y-0 left-0 z-30 w-64 bg-white dark:bg-gray-900 transition-colors dark:bg-gray-900 shadow-lg transition-colors transform ${
+        className={`fixed inset-y-0 left-0 z-30 w-64 bg-white dark:bg-gray-900 transition-colors shadow-lg transform ${
           isOpen ? 'translate-x-0' : '-translate-x-full'
         } md:translate-x-0 transition-transform duration-300 ease-in-out md:relative md:z-0`}
       >
