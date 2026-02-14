d09aa9eb2547db10a32cd3f3ee5458a09b085713
merdy-pharma <fifinalandu@gmail.com>
2026-02-14 19:21:54 +0100

    Updated Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -195,7 +195,7 @@ import { useAuth } from "@/hooks/useAuth";
       <div className="flex items-center justify-between">
         <div>
           <h1 className="text-2xl font-bold dark:text-gray-400">Point de Vente - Dashboard</h1>
-          <p className="text-sm text-gray-500">Bienvenue {user?.name} !</p>
+          <p className="text-sm dark:text-gray-500">Bienvenue {user?.name} !</p>
         </div>
       </div>
 
