55202192fa9cab781408b26ef05e9d01623409de
merdy-pharma <fifinalandu@gmail.com>
2026-02-14 20:00:40 +0100

    Updated Sales.tsx

--- src/pages/Sales.tsx
+++ src/pages/Sales.tsx
@@ -397,7 +397,7 @@ const Sales: React.FC = () => {
             <input
               type="text"
               placeholder="Entrez le nom de l'article ou son barcode..."
-              className="w-full pl-10 pr-4 py-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500"
+              className="w-full pl-10 pr-4 py-2 border border-gray-200 dark:border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
             />
