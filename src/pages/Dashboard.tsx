e459fa9d987fc9e5df49f7a3cfe8b7e487c955b8
merdy-pharma <fifinalandu@gmail.com>
2026-02-14 19:22:27 +0100

    Updated Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -251,7 +251,7 @@ import { useAuth } from "@/hooks/useAuth";
               <div className="bg-white rounded-lg shadow-sm p-6">
                 <div className="flex items-center justify-between mb-6">
                   <div>
-                    <h2 className="text-lg font-semibold text-gray-900">Vue des ventes par jour</h2>
+                    <h2 className="text-lg font-semibold dark:text-gray-500">Vue des ventes par jour</h2>
                     <p className="text-sm text-gray-500">Performance 7 derniers jours</p>
                   </div>
                 </div>
