539721ea7254f48313c7c3f6df940a5edf99c6bb
merdy-pharma <fifinalandu@gmail.com>
2026-02-14 18:28:21 +0100

    Updated Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -18,7 +18,8 @@ const Layout: React.FC = () => {
       <div className="flex flex-col flex-1 overflow-hidden">
         <Header toggleSidebar={toggleSidebar} />
         
-        <main className="flex-1 overflow-y-auto p-4 md:p-6">
+       
+        <main className="flex-1 overflow-y-auto p-4 md:p-6 bg-white dark:bg-gray-800 transition-colors">  
           <div className="max-w-7xl mx-auto">
             <Outlet />
           </div>
