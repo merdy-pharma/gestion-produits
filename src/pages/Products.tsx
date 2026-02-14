26069e89c9d626d85bf4038b15d61873e8ea67f7
merdy-pharma <fifinalandu@gmail.com>
2026-02-14 20:26:32 +0100

    Updated Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -460,7 +460,7 @@ return (
 
       {/* HEADER + EXPORT BUTTONS 
       <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-3"> */}
-      <div className="flex flex-col md:flex-row gap-6 p-4 bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-200 min-h-screen transition-colors">  
+      <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-3 gap-6 p-4 bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-200 min-h-screen transition-colors">  
 
       <h1 className="text-xl sm:text-2xl font-bold">Tous nos Articles</h1>
     
