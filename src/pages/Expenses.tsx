8b55f326517c604fc0c177289681cab23abea209
merdy-pharma <fifinalandu@gmail.com>
2026-02-16 16:39:11 +0100

    Updated Expenses.tsx

--- src/pages/Expenses.tsx
+++ src/pages/Expenses.tsx
@@ -543,7 +543,7 @@ export default function Expenses() {
          MODAL — CATEGORY MANAGER
       ============================================ */}
       {showCategoryManager && (
-        <div className="fixed inset-0 bg-black bg-opacity-40 flex items-start justify-center z-50 p-3 overflow-y-auto">
+        <div className="modal-overlay">
           <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-3xl relative mt-10">
 
             <button
