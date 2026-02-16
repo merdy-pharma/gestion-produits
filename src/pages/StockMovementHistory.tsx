d34471826d7f4f9be811d7eccbae96bc644a7702
merdy-pharma <fifinalandu@gmail.com>
2026-02-16 14:35:14 +0100

    Updated StockMovementHistory.tsx

--- src/pages/StockMovementHistory.tsx
+++ src/pages/StockMovementHistory.tsx
@@ -183,7 +183,7 @@ export default function StockMovementsHistory() {
       </div>
 
       {/* TABLE */}
-      <table className="w-full text-sm border">
+      <table className="table-content">
         <thead className="table-head">
           <tr>
             <th className="p-2">Date</th>
