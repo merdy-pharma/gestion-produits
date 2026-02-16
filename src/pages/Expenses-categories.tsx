470e0f8566ca2c173e7e46c05de68b1ac41c8831
merdy-pharma <fifinalandu@gmail.com>
2026-02-16 17:13:39 +0100

    Updated Expenses-categories.tsx

--- src/pages/Expenses-categories.tsx
+++ src/pages/Expenses-categories.tsx
@@ -69,7 +69,7 @@ export default function ExpenseCategories() {
         </button>
       </div>
 
-      <div className="page">
+      <div className="page overflow-x-auto shadow rounded-lg border border-gray-200">
         <table className="table-contenair min-w-full">
           <thead className="table-head">
             <tr>
