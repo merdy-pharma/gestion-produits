97ecc2f8275d9467208447106c977df1e018eb4f
merdy-pharma <fifinalandu@gmail.com>
2026-02-16 16:47:04 +0100

    Updated Expenses-categories.tsx

--- src/pages/Expenses-categories.tsx
+++ src/pages/Expenses-categories.tsx
@@ -63,7 +63,7 @@ export default function ExpenseCategories() {
 
         <button
           onClick={openCreate}
-          className="flex items-center gap-2 px-3 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition"
+          className="btn-secondary flex items-center gap-2 px-3 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition"
         >
           <PlusCircle size={18} /> Ajouter
         </button>
