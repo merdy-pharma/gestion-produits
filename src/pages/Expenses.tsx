cb8840a490e5fe1ebd215ce87be0acc7ef1c2165
merdy-pharma <fifinalandu@gmail.com>
2026-02-16 16:34:14 +0100

    Updated Expenses.tsx

--- src/pages/Expenses.tsx
+++ src/pages/Expenses.tsx
@@ -531,7 +531,7 @@ export default function Expenses() {
             {/* SAVE */}
             <button
               onClick={handleSaveWithConversion}
-              className="btn-success mt-4 w-full  text-white py-2 rounded hover:bg-blue-700"
+              className="btn-primary mt-4 w-full  text-white py-2 rounded hover:bg-blue-700"
             >
               Enregistrer
             </button>
