ce3c22b9ea336dd9174bb7959fa55e0013b85f11
merdy-pharma <fifinalandu@gmail.com>
2026-02-17 11:47:58 +0100

    Updated ProfilesPage.tsx

--- src/pages/ProfilesPage.tsx
+++ src/pages/ProfilesPage.tsx
@@ -306,7 +306,7 @@ export default function ProfilesPage() {
                 <button
                   onClick={() => setPage(page + 1)}
                   disabled={profiles.length < perPage}
-                  className="px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed"
+                  className="px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed dark:hover:bg-gray-300"
                 >
                   Suivant
                 </button>
