b2ed2214043b35e48e709a9468a6a8b89a233df4
merdy-pharma <fifinalandu@gmail.com>
2026-02-17 16:48:37 +0100

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -54,7 +54,7 @@ const Header: React.FC<HeaderProps> = ({ toggleSidebar }) => {
         <div className="flex items-center space-x-4">
           <button
            onClick={toggleTheme}
-            className={`relative inline-flex h-8 w-16 items-center rounded-full transition-colors duration-300 ${
+            className={`relative inline-flex h-5 w-10 items-center rounded-full transition-colors duration-300 ${
               theme === 'dark' ? 'bg-slate-700' : 'bg-gray-700'
             }`}
             aria-label="Basculer le thème"
