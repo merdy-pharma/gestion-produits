96f299b328464289315236c40df0a4d32562f04b
merdy-pharma <fifinalandu@gmail.com>
2026-02-17 11:55:07 +0100

    Updated ProfilesPage.tsx

--- src/pages/ProfilesPage.tsx
+++ src/pages/ProfilesPage.tsx
@@ -477,7 +477,7 @@ function ProfileForm({
                   if (errors.name) setErrors({ ...errors, name: '' });
                 }}
                 placeholder="Nom complet"
-                className={`w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 ${
+                className={`input w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 ${
                   errors.name
                     ? 'border-red-500 focus:ring-red-500'
                     : 'border-gray-300 focus:ring-blue-500'
