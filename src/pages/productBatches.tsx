1c5b73a4bc5d77efa56694452c3607059b24384c
merdy-pharma <fifinalandu@gmail.com>
2026-02-18 22:13:29 +0100

    Updated productBatches.tsx

--- src/pages/productBatches.tsx
+++ src/pages/productBatches.tsx
@@ -169,10 +169,10 @@ export default function ProductBatchesPage() {
   return (
     <div className="page p-6 max-w-7xl mx-auto">
       <h1 className="text-2xl font-bold mb-6">Gestion des Lots</h1>
-      <span className="fixed top-0 right-0 m-4 z-50">
+      <span className="fixed top-5 right-0 m-4 z-50">
         <button
                 onClick={() => navigate("/products")}
-                className="btn-danger"
+                className="btn-danger rounded-full p-2 shadow"
               >
                 <X size={18} />
          </button>
