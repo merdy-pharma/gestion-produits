fb59348f8174109ee66c7ee16321f1b74dbbda10
merdy-pharma <fifinalandu@gmail.com>
2026-02-26 15:02:41 +0100

    Updated productBatches.tsx

--- src/pages/productBatches.tsx
+++ src/pages/productBatches.tsx
@@ -355,7 +355,7 @@ export default function ProductBatchesPage() {
               <th className="p-2 border">Expiration</th>
               <th className="p-2 border">Quantité</th>
               <th className="p-2 border">Prix achat</th>
-              <th className="p-3 border">Statut</th>
+              <th className="p-2 border">Statut</th>
               <th className="p-2 border">Actions</th>
             </tr>
           </thead>
