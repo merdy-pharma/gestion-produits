6cf027f7a2f7ac34d538832f96cfdeed1151dc04
merdy-pharma <fifinalandu@gmail.com>
2026-02-26 14:31:14 +0100

    Updated productBatches.tsx

--- src/pages/productBatches.tsx
+++ src/pages/productBatches.tsx
@@ -425,7 +425,7 @@ export default function ProductBatchesPage() {
           <button
             disabled={page === 1}
             onClick={() => setPage(page - 1)}
-            className="px-3 py-1 border rounded disabled:opacity-50"
+            className="px-3 py-1 border rounded bg-gray-200 dark:bg-gray-700 disabled:opacity-50"
           >
             Précédent
           </button>
