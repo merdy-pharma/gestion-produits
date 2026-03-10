7d266ae0be614edd9f01caf2758b2c0943358550
merdy-pharma <fifinalandu@gmail.com>
2026-03-10 11:07:34 +0100

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -61,7 +61,7 @@ const Header: React.FC<HeaderProps> = ({ toggleSidebar }) => {
           >
             <span
               className={`inline-block h-4 w-4 transform rounded-full bg-white shadow-lg transition-transform duration-300 ${
-                theme === 'dark' ? 'translate-x-7' : 'translate-x-0.5'
+                theme === 'dark' ? 'translate-x-6' : 'translate-x-0.5'
               }`}
             />
           </button>
