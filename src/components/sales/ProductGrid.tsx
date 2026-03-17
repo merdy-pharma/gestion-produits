322b5fc16aace9fa6a8dc42615c2f255fddaa20e
merdy-pharma <fifinalandu@gmail.com>
2026-03-17 18:58:39 +0100

    Updated ProductGrid.tsx

--- src/components/sales/ProductGrid.tsx
+++ src/components/sales/ProductGrid.tsx
@@ -98,7 +98,7 @@ const ProductGrid: React.FC<ProductGridProps> = ({
                     ${isOutOfStock ? 'opacity-50 cursor-not-allowed' : ''}
                     
                     ${inCart
-                      ? 'bg-primary-50 dark:bg-yellow-700/30'
+                      ? 'bg-primary-50 dark:bg-yellow-400/30'
                       : 'hover:bg-gray-50 dark:hover:bg-gray-700'}
                   `}
                 >
