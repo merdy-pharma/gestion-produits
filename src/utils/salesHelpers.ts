65ec77955d279b9ba9a105b54013469306c866ff
merdy-pharma <fifinalandu@gmail.com>
2026-03-17 16:41:39 +0100

    Updated salesHelpers.ts

--- src/utils/salesHelpers.ts
+++ src/utils/salesHelpers.ts
@@ -94,7 +94,7 @@ export function addProductToCart(
 
   if (existingItem) {
     return cart.map((item) =>
-      item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+      item.id === product.id ? { ...item, quantity: Number(item.quantity) + 1 } : item
     );
   }
 
