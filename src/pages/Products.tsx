415fa2bc98d81077d53a2f026defc308796093c4
merdy-pharma <fifinalandu@gmail.com>
2026-03-20 15:01:17 +0100

    Updated Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -839,7 +839,7 @@ return (
               </div>
               <button
               onClick={() => navigate(`/productBatches?product=${product.id}`)}
-              className="w-full bg-primary-500 text-xs text-purple-600 hover:underline"
+              className="w-full bg-primary-500 text-xs text-purple-600 rounded "
             >
               Gérer les lots
             </button>
